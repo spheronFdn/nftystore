1e04ae861df96b15f2087fcd5803e4b6b3143e12
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2022-12-15 20:46:37 +0530

    Added dropzone package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import Dropzone from "./components/dropzone";
 
 function App() {
-  return <div className="App">NFT</div>;
+  return (
+    <div className="App">
+      NFT
+      <Dropzone />
+    </div>
+  );
 }
 
 export default App;
