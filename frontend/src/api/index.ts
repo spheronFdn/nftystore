e8477b47a116e477831abb1449d338af21e1eaa4
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2022-12-29 20:24:21 +0530

    Configured base64

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,14 +2,11 @@ import axios from "axios";
 
 const BASE_URI = "http://localhost:8088";
 
-export const uploadFiles = async (
-  protocol: string,
-  files: Array<Blob> | Array<MediaSource>
-) => {
+export const uploadFiles = async (protocol: string, files: any) => {
   try {
     const response = await axios({
       url: `${BASE_URI}/uploadCollection`,
-      data: { files, protocol },
+      data: { files: files.map((file: any) => file.preview), protocol },
       method: "POST",
       onUploadProgress: (progress: any) => {
         console.log(progress);
