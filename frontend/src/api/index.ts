4aacdec46f2c211a238a593e69779c18c78c00d1
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2022-12-30 02:11:22 +0530

    Added formdata in request

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -3,10 +3,15 @@ import axios from "axios";
 const BASE_URI = "http://localhost:8088";
 
 export const uploadFiles = async (protocol: string, files: any) => {
+  const formdata = new FormData();
+  files.forEach((file: any) => {
+    formdata.append(file.path, file.preview);
+  });
+  console.log("FORM DATA1: ", formdata);
   try {
     const response = await axios({
       url: `${BASE_URI}/uploadCollection?protocol=${protocol}`,
-      data: { files: files.map((file: any) => file.preview), protocol },
+      data: formdata,
       method: "POST",
       onUploadProgress: (progress: any) => {
         console.log(progress);
