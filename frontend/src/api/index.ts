6aa38fca9fecdf926e46e805fe65551cc17682d8
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2022-12-29 21:23:44 +0530

    Updated upload endpoint

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -5,7 +5,7 @@ const BASE_URI = "http://localhost:8088";
 export const uploadFiles = async (protocol: string, files: any) => {
   try {
     const response = await axios({
-      url: `${BASE_URI}/uploadCollection`,
+      url: `${BASE_URI}/uploadCollection?protocol=${protocol}`,
       data: { files: files.map((file: any) => file.preview), protocol },
       method: "POST",
       onUploadProgress: (progress: any) => {
