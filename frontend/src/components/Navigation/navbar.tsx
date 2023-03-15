9b79662b0db1b0c15d84d2125341bbccd46b8d54
Kanika Agrawal <kanikaagrawal@Kanikas-MacBook-Pro.local>
2023-03-15 19:24:22 +0530

    Add route to home button

--- frontend/src/components/Navigation/navbar.tsx
+++ frontend/src/components/Navigation/navbar.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { ReactComponent as SpheronLogo } from "../../assets/icons/spheron-logo.svg";
 import NavStyle from "../../styles/navigation.module.css";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <nav className={NavStyle.navbar}>
-      <SpheronLogo />
+      <div onClick={() => navigate("/")} className={NavStyle.logo}>
+        <SpheronLogo />
+      </div>
     </nav>
   );
 };
